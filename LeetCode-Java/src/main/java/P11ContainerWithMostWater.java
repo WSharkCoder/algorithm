e9ba202a832de610f6480e68a5e5fc63113a7bb2
WSharkCoder <fangjunxiong@outlook.com>
2025-03-30 19:23:45 +0800

    chore: 刷题打卡+1

---
+++ LeetCode-Java/src/main/java/P11ContainerWithMostWater.java
@@ -0,0 +1,15 @@
+/**
+ * Created by WSharkCoder on 2025/03/30. <br/>
+ * 11. 盛最多水的容器
+ *
+ * @author WSharkCoder
+ * @date 2025/03/30
+ */
+public class P11ContainerWithMostWater {
+    class Solution {
+        public int maxArea(int[] height) {
+
+            return 0;
+        }
+    }
+}
--- LeetCode-Java/src/main/java/P1TwoSum.java
+++ LeetCode-Java/src/main/java/P1TwoSum.java
@@ -1,11 +1,23 @@
 import java.util.HashMap;
 
 /**
+ * 1.两数之和
+ *
  * @author WSharkCoder
  * @date 2022/03/07
  */
 public class P1TwoSum {
-    class Solution {
+
+    static class Solution {
+        /**
+         * 解题思路:
+         * 倒序索引 map<value,index>，遍历每个数据，判断另外一个（相加=target）值是否在倒序索引中
+         * 时间复杂度: O(n)
+         *
+         * @param nums   数据数组
+         * @param target 目标值
+         * @return 下标
+         */
         public int[] twoSum(int[] nums, int target) {
             // val->index映射
             HashMap<Integer, Integer> map = new HashMap<>();
